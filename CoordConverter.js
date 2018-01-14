c555001a4afc3686b7e8c1413f5ecfc4c07a5cae
Schuyler Ankele <schuyler.ankele@gmail.com>
2018-01-14 03:12:49 -0700

    String parsing complete.  Testing tomo

--- CoordConverter.js
+++ CoordConverter.js
@@ -1,7 +1,18 @@
 
-var coordinateObj = {
+function CoordinateObj() {
 
-};
+}
+
+function DatumCoordinateObj() {
+
+}
+
+function DatumObj(degrees, minutes, seconds, hemisphere) {
+    this.degrees = degrees;
+    this.minutes = minutes;
+    this.seconds = seconds;
+    this.hemisphere = hemisphere;
+}
 
 
 /**
@@ -18,6 +29,7 @@ function stdCoordsToDecimal(dd, mm, ss, isNegative = false){
 
     let decimalFmt = dd + mm/60 +ss/3600;
     if(isNegative){
+        decimalFmt = decimalFmt * -1;
         console.log(isNegative);
     }
     return decimalFmt.toFixed(12);
@@ -28,8 +40,34 @@ function decimalCoordsToStd(){
 }
 
 function datumFromString(datumToParse){
-
+    let stingArray = datumToParse.split(" ");
+    let degrees =  parseFloat(stingArray[0].replace(/d|e|g|r|s|°/gi, " "));
+    let mins = parseFloat(stingArray[1].replace(/m|i|n|u|t|e|s|"/gi, " "));
+    let secs = parseFloat(stingArray[2].replace(/s|e|c|o|n|d|"/gi, " "));
+    /*
+    for(let i=0; i < degrees.length; i++){
+        let checkChar = degrees[i];
+        // Lets clean up the chars
+        if(checkChar == "d"|| checkChar == "e"|| checkChar == "g"||
+            checkChar == "r"|| checkChar == "s" || checkChar == "°"){
+            console.log(degrees[i]);
+            degrees.replace(checkChar, " ");
+        }
+    }
+    */
+    degrees=degrees*2;
+    mins=mins*2;
+    secs=secs*2;
+    console.log(degrees);
+    console.log(mins);
+    console.log(secs);
+    /*console.log(stingArray[0]);
+    console.log(stingArray[1]);
+    console.log(stingArray[2]);
+    console.log(stingArray[2][0]);
+    */
 }
 
 console.log(stdCoordsToDecimal(35, 54, 23, true));
 //console.log(stdCoordsToDecimal(-35, 54, 23));
+datumFromString("35° 54' 22.9998\"");
