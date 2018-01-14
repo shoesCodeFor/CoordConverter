c68b09b32e324fccf8e706198589abe7e74d3f47
Schuyler Ankele <schuyler.ankele@gmail.com>
2018-01-13 23:58:05 -0700

    More conversion specs

--- CoordConverter.js
+++ CoordConverter.js
@@ -1,10 +1,21 @@
+
+var coordinateObj = {
+    
+};
+
+
 /**
  *  (dd + mm/60 +ss/3600) to Decimal degrees (dd.ff)
  *   dd = whole degrees, mm = minutes, ss = seconds
  *
  *   .60798357841 11 places is most accurate
  */
-function stdCoordsToDecimal(dd, mm, ss, isNegative=false){
+function stdCoordsToDecimal(dd, mm, ss, isNegative = false){
+    let degrees = parseFloat(dd);
+    let minutes = parseFloat(mm);
+    let seconds = parseFloat(ss);
+    let hemisphere = isNegative;
+
     let decimalFmt = dd + mm/60 +ss/3600;
     if(isNegative){
         console.log(isNegative);
@@ -16,5 +27,9 @@ function decimalCoordsToStd(){
 
 }
 
+function datumFromString(datumToParse){
+
+}
+
 console.log(stdCoordsToDecimal(35, 54, 23, true));
-console.log(stdCoordsToDecimal(-35, 54, 23));
+//console.log(stdCoordsToDecimal(-35, 54, 23));
