6e68331190df852e089629a2bf70f5e3c751fa59
Schuyler Ankele <schuyler.ankele@gmail.com>
2018-01-28 10:46:32 -1000

    Testing skeleton

--- tests.js
+++ tests.js
@@ -4,4 +4,18 @@
 
 const CoordConverterTests = require('CoordConverter.js');
 
+// Log out all functions
 console.log(CoordConverterTests);
+
+
+// Test for single std format coord
+
+// Test for single decimal format pair
+
+// Test for standard coord pair to decimal format with spaces
+
+// Test for standard coords pair to decimal format no spaces
+
+// Test for decimal format coord pair to standard coords
+
+
