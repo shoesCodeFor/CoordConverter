7156f142cb2abec4021c91d2970bc59eff7883dd
Schuyler Ankele <schuyler.ankele@gmail.com>
2018-02-01 19:07:56 -1000

    Testing docs from ad-hoc stuff

--- CoordConvModule.js
+++ CoordConvModule.js
@@ -273,30 +273,3 @@ module.exports = CoordConversion;
 
 
 
-
-/* Tests
-// Std Coords to Decimal
-console.log(stdCoordsToDecimal(35, 54, 23, true));
-//console.log(stdCoordsToDecimal(-35, 54, 23));
-
-/* Datum from a string into conversion
-
-datumFromString("35° 54' 22.9998\"");
-var tester = datumFromString("35° 54' 22.9998\"W");
-console.log(stdCoordsToDecimal(tester.degrees, tester.minutes, tester.seconds, tester.hemisphere));
-
-*/
-
-/* The whole shebang
-var testStr = "35° 54' 22.9998\"W 35° 54' 22.9998\"";
-var num_matches = testStr.match(/ /gi).length;
-console.log("Number of spaces: " + num_matches);
-
-*/
-
-
-// var TestCoord = decimalCoordsToStd(34.7663, false);
-// console.log(TestCoord);
-
-// Error test
-// decimalCoordsToStd("93kkd");
